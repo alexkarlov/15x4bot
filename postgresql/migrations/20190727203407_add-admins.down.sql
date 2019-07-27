b08936442e0cb0c61f2ece62a7dc71a2cf885f95
okarlov <okarlov@odl1902009.lohika.com>
2019-07-28 00:05:30 +0300

    add new admins

---
+++ postgresql/migrations/20190727203407_add-admins.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM users WHERE username IN ('a_petrenko', 'VladislavVodko');
---
+++ postgresql/migrations/20190727203407_add-admins.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, role, name, fb, bdate) 
+VALUES ('a_petrenko', 'admin', 'Alex', 'https://www.facebook.com/p.alexandr.od', NULL),
+('VladislavVodko', 'admin', 'Влад', 'https://www.facebook.com/vladislavvodko', NULL);
