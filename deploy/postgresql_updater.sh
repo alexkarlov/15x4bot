a2f13ebd21be07514caf909dd2921f8524b37bb4
ilya-korotya <officalbatya@gmail.com>
2019-08-14 16:51:31 +0300

    update postgresql after diff

---
+++ deploy/postgresql_updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+IS_EXIST="$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep postgresql)"
+if [ -n "$IS_EXIST" ]
+then
+  echo "Start update postgresql ..."
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml up bot-db --force-recreate -d
+  docker run -v /home/ec2-user/postgresql/migrations:/migrations --network=15x4  migrate/migrate -path=/migrations/ -database postgres://bot:@bot-db:5432/bot?sslmode=disable up
+  echo "Finish update postgresql"
+fi
