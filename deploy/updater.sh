bc53022856471d950f3e9bb802db366c953d0772
ilya-korotya <officalbatya@gmail.com>
2019-08-14 17:52:59 +0300

    add commit range

--- deploy/updater.sh
+++ deploy/updater.sh
@@ -3,6 +3,7 @@ IS_EXIST="$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep postgresql)"
 if [ -n "$IS_EXIST" ]
 then
   # Update postgresql and bot
+  echo $TRAVIS_COMMIT_RANGE
   echo "Start update postgresql and bot ..."
   ssh -i "travis.pem" -t $SERVER_USER@$SERVER_DNS "docker-compose -f docker-compose.yml -f docker-compose.production.yml up --force-recreate -d"
   echo "Finish update postgresql and bot"
