fbe4956de6142095f0f4bf4a42abcb83758f5d97
ilya-korotya <officalbatya@gmail.com>
2019-08-14 17:10:15 +0300

    fix commands for ci cd pipeline

--- deploy/postgresql_updater.sh
+++ deploy/postgresql_updater.sh
@@ -3,7 +3,7 @@ IS_EXIST="$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep postgresql)"
 if [ -n "$IS_EXIST" ]
 then
   echo "Start update postgresql ..."
-  docker-compose -f docker-compose.yml -f docker-compose.production.yml up postgresql --force-recreate -d
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml up --force-recreate -d postgresql
   docker run -v /home/ec2-user/postgresql/migrations:/migrations --network=15x4  migrate/migrate -path=/migrations/ -database postgres://bot:@bot-db:5432/bot?sslmode=disable up
   echo "Finish update postgresql"
 fi
